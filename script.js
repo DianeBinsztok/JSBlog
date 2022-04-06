790c966f9d08204559b1bc4808cf687c7825897c
DianeBinsztok <diane.binsztok@le-campus-numerique.fr>
2022-04-06 13:18:51 +0200

    feat: Refresh button on click: function reload()

--- script.js
+++ script.js
@@ -2,52 +2,59 @@ $(document).ready(function () {
     const src = "https://chroniclingamerica.loc.gov/search/titles/results/?terms=michigan&format=json&page=5";
     console.log(src);
 
+   
+   // Pour rafraichir le feed:
+    function reload() {
+        const request = $.ajax({
+            url: src,
+            method: "GET",
+            dataType:"json",
+        }).done(function (response) {
+    
+            //J'ajoute une liste dans le contenu html
+            let list = $("<ul>").addClass("list");
+            $("#content").append(list);
+    
+    
+            let i = 1;
+            for (item of response.items) {
+    
+              // Pour chaque élément du JSON:
+                // Je génère une puce de la liste
+                let listItem = $("<li>").addClass("list_item");
+    
+                // Je génère un post
+                let newPost = generatePost(item);
+                // J'ajoute une image
+                let postImg = $("<img>").attr("src", "./img/" + i + ".jpg");
+                
+                listItem.append(postImg);
+                listItem.append(newPost);
+                list.append(listItem);
+    
+                i++;
+            }
+        }).fail(function(error){
+            alert("La requête s'est terminée en échec. Infos : " + JSON.stringify(error));
+        }).always(function(){
+            console.log("Requête effectuée");
+        });;
+    }
+
+
     // Fonction pour générer un post
     function generatePost(article) {
         let post = $("<article>").addClass("post");    
         let postTitle = $("<h3>").addClass("post_title").text(article.city[0]);
         let postYear = $("<h4>").addClass("post_year").text("Published in: " + article.start_year);
-        let postContent = $("<p>").addClass("post_content").text("Lorem ipsum blabla blablabla blabla blablabla");
+        let postContent = $("<p>").addClass("post_content").text("Article publié a " +article.city + ", en " + article.start_year+ " par "+ article.publisher, "+ ", article.note[0]);
        
         post.append(postTitle, postTitle, postYear, postContent);
-        console.log(post[0]);
         return post[0];
     } 
 
-    const request = $.ajax({
-        url: src,
-        method: "GET",
-        dataType:"json",
-    }).done(function (response) {
+    $('#refresh_button').click(function(){reload()});
 
-        //J'ajoute une liste dans le contenu html
-        let list = $("<ul>").addClass("list");
-        $("#content").append(list);
-
-
-        let i = 1
-            ;
-        for (item of response.items) {
-
-          // Pour chaque élément du JSON:
-            // Je génère une puce de la liste
-            let listItem = $("<li>").addClass("list_item");
-
-            // Je génère un post
-            let newPost = generatePost(item);
-        // J'ajoute une image
-            let postImg = $("<img>").attr("src", "./img/" + i + ".jpg");
-            
-            listItem.append(postImg);
-            listItem.append(newPost);
-            list.append(listItem);
-
-            i++;
-        }
-    }).fail(function(error){
-        alert("La requête s'est terminée en échec. Infos : " + JSON.stringify(error));
-    }).always(function(){
-        console.log("Requête effectuée");
-    });;
+  
 });
 
