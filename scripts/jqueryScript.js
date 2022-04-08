83a78c1862264f884af99e87847c63d1f740faf3
DianeBinsztok <diane.binsztok@le-campus-numerique.fr>
2022-04-08 10:55:42 +0200

    fix: scripts renamed, files in order

---
+++ scripts/jqueryScript.js
@@ -0,0 +1,64 @@
+$(document).ready(function () { 
+    const src = "https://chroniclingamerica.loc.gov/search/titles/results/?terms=michigan&format=json&page=5";
+    console.log(src);
+
+   
+   // Pour rafraichir le feed:
+    function reload() {
+
+        // requête à l'API
+        const request = $.ajax({
+            url: src,
+            method: "GET",
+            dataType:"json",
+        }).done(function (response) {
+    
+            //J'ajoute une liste dans le contenu html
+            let list = $("<ul>").addClass("list");
+            $("#content").append(list);
+    
+    
+            let i = 1;
+            for (item of response.items) {
+    
+              // Pour chaque élément du JSON:
+                // Je génère une puce de la liste
+                let listItem = $("<li>").addClass("list_item");
+    
+                // Je génère un post
+                let newPost = generatePost(item);
+
+                // J'ajoute une image
+                let postImg = $("<img>").attr("src", "./img/" + i + ".jpg");
+                
+                listItem.append(postImg);
+                listItem.append(newPost);
+                list.append(listItem);
+    
+                i++;
+            }
+        }).fail(function (error) {
+            let failMsg = $("<h3>").text("La requête s'est terminée en échec. Infos : " + JSON.stringify(error));
+            $("content").failMsg;
+        }).always(function(){
+            console.log("Requête effectuée");
+        });;
+    }
+
+
+    // Fonction pour générer un post
+    function generatePost(article) {
+        let post = $("<article>").addClass("post");    
+        let postTitle = $("<h3>").addClass("post_title").text(article.city[0]);
+        let postYear = $("<h4>").addClass("post_year").text("Published in: " + article.start_year);
+        let postContent = $("<p>").addClass("post_content").text("Article publié a " +article.city + ", en " + article.start_year+ " par "+ article.publisher, "+ ", article.note[0]);
+       
+        post.append(postTitle, postTitle, postYear, postContent);
+        return post[0];
+    } 
+
+    $('#refresh_button').click(function(){reload()});
+
+  
+});
+
---
+++ scripts/script.js
@@ -0,0 +1,94 @@
+const src =
+  "https://chroniclingamerica.loc.gov/search/titles/results/?terms=michigan&format=json&page=5";
+console.log(src);
+
+let content = document.getElementById("content");
+let list = document.createElement("ul");
+content.appendChild(list);
+
+// Générer un post
+function generatePost(article) {
+  console.log(article);
+  //balise <article>
+  let post = document.createElement("article");
+  post.classList.add("post");
+
+  //<h3> : titre de l'article
+  let postTitle = document.createElement("h3");
+  postTitle.classList.add("post_title");
+  postTitle.innerHTML = article.city[0];
+
+  //<h4> : l'année de parution
+  let postYear = document.createElement("h4");
+  postYear.classList.add("post_year");
+  postYear.innerHTML = article.start_year;
+
+  //<p> : le contenu de l'article
+  let postContent = document.createElement("p");
+  postContent.classList.add("post_content");
+  (postContent.innerText =
+    "Article publié a " +
+    article.city +
+    ", en " +
+    article.start_year +
+    " par " +
+    article.publisher),
+    "+ ",
+    article.note[0];
+  // Le tout dans <article>
+  post.appendChild(postTitle);
+  post.appendChild(postYear);
+  post.appendChild(postContent);
+  return post;
+}
+
+// Générer une liste de posts (en intégrant les images)
+function generatePostsList(articles) {
+  // pour chaque item reçu dans handleResponse()
+  for (let i = 1; i < articles.items.length; i++) {
+    // Pour chaque élément du JSON:
+
+    // <li> : Je génère une puce de la liste
+    let listItem = document.createElement("li");
+    listItem.classList.add("list_item");
+
+    // Je génère un post
+    let newPost = generatePost(articles.items[i]);
+
+    // J'ajoute une image
+    let postImg = document.createElement("img");
+    postImg.setAttribute("src", `./img/${i}.jpg`);
+
+    // Je lie le tout à la puce
+    listItem.appendChild(postImg);
+    listItem.appendChild(newPost);
+    list.appendChild(listItem);
+  }
+}
+
+document.getElementById("refresh_button").onclick = reload;
+//Pour rafraichir le feed: requête à l'API:
+function reload() {
+  // fetch().then() peut aussi s'écrire avec asynch().await()
+  fetch(src)
+    .then(function handleResponse(response) {
+      // renvoie une promesse: pas encore un résultat
+      if (!response.ok) {
+        throw new Error(
+          `Erreur ${response.status}: La requête à l' API a échoué.`
+        );
+      } else {
+        return response.json();
+      }
+    })
+    .then(
+      // récupère le retour du .then() précédent: doit être un .json()
+      generatePostsList
+    )
+    .catch(function handleError(error) {
+      let failMsg = document.createElement("h3");
+      failMsg.innerText = `La requête s'est soldée par un échec : ${JSON.stringify(
+        error
+      )}`;
+    });
+}
