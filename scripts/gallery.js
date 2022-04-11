a82b88f22d13588e9aeb45174d11707a6f29c964
DianeBinsztok <diane.binsztok@le-campus-numerique.fr>
2022-04-11 11:32:15 +0200

    style: gallery bassic css - #8

--- scripts/gallery.js
+++ scripts/gallery.js
@@ -3,11 +3,14 @@ function displayImages() {
   let content = document.getElementById("content");
   // le container de la galerie;
   let gallery = document.createElement("div");
-  gallery.className = "gallery";
+  gallery.id = "gallery";
 
   for (i = 1; i < 31; i++) {
     let img = document.createElement("img");
+    img.className = "image";
     img.setAttribute("src", `./img/${i}.jpg`);
+    img.setAttribute("alt", "Bruce Lee being awsome");
+
     gallery.appendChild(img);
   }
 
