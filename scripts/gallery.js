c1cefae56128ce7c32185f9117c9f24723df50ad
DianeBinsztok <diane.binsztok@le-campus-numerique.fr>
2022-04-12 09:32:21 +0200

    feat: Toggle gallery from vertical to mosaic display - #8

--- scripts/gallery.js
+++ scripts/gallery.js
@@ -1,9 +1,11 @@
+// 1 - gallery:
 function displayImages() {
   // le root:
   let content = document.getElementById("content");
   // le container de la galerie;
   let gallery = document.createElement("div");
   gallery.id = "gallery";
+  gallery.className = "vertical";
 
   for (i = 1; i < 31; i++) {
     let img = document.createElement("img");
@@ -16,4 +18,18 @@ function displayImages() {
 
   content.appendChild(gallery);
 }
+
+// 2 - toggle button:
+let displayBtn = (document.getElementById("display_btn").onclick =
+  toggleDisplay);
+
+function toggleDisplay() {
+  let gallery = document.getElementById("gallery");
+  if (gallery.className == "vertical") {
+    gallery.className = "mosaic";
+  } else if (gallery.className == "mosaic") {
+    gallery.className = "vertical";
+  }
+}
+
 displayImages();
